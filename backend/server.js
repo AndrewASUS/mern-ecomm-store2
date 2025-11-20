6d395f670790c537b5b4fe6a306d2ba7e23cd37d
Andrew Walton <163466371+AndrewASUS@users.noreply.github.com>
2025-11-20 20:10:34 +1100

    Test 2

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,43 @@
 import express from "express";
-import "dotenv/config";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import path from "path";
-import { fileURLToPath } from "url";
 
-import paymentRoutes from "./routes/payment.route.js";
-import couponRotes from "./routes/coupon.route.js";
-import cartRoutes from "./routes/cart.route.js";
-import productRoutes from "./routes/product.route.js";
 import authRoutes from "./routes/auth.route.js";
-import analiticsRoutes from "./routes/analytics.route.js";
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
+
 import { connectDB } from "./lib/db.js";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Proper __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
-app.use(express.json({ limit: "10mb" })); // Allows parsing to the body of the request
-app.use(cookieParser()); // Cookie parser middleware
+app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
+app.use(cookieParser());
 
-// API routes
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
-app.use("/api/coupons", couponRotes);
+app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
-app.use("/api/analytics", analiticsRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
-// Production build
 if (process.env.NODE_ENV === "production") {
-  // Serve frontend dist folder
-  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  // Handle SPA routing
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-  });
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
 app.listen(PORT, () => {
-  console.log("Server is running on port: " + PORT);
-  connectDB();
+	console.log("Server is running on http://localhost:" + PORT);
+	connectDB();
 });
