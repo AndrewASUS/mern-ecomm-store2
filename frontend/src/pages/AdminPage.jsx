c116fa9cdec0645ad7a315995ff60d1a391a9735
Andrew Walton <163466371+AndrewASUS@users.noreply.github.com>
2025-11-24 18:11:19 +1100

    Updated small screens styling for admin page

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -15,15 +15,11 @@ const tabs = [
 const AdminPage = () => {
   const [activeTab, setActiveTab] = useState("create");
 
-
-  const { fetchAllProducts } = useProductStore()
-
+  const { fetchAllProducts } = useProductStore();
 
   useEffect(() => {
-    fetchAllProducts()
-  }, [fetchAllProducts])
-
-  
+    fetchAllProducts();
+  }, [fetchAllProducts]);
 
   return (
     <div className="min-h-screen relative overflow-hidden">
@@ -42,22 +38,21 @@ const AdminPage = () => {
             <button
               key={tab.id}
               onClick={() => setActiveTab(tab.id)}
-              className={`flex items-center px-4 py-2 mx-2 rounded-md transition-colors duration-200 ${
+              className={`flex items-center justify-center px-4 py-2 rounded-md transition-colors duration-200 ${
                 activeTab === tab.id
                   ? "bg-emerald-600 text-white"
                   : "bg-gray-700 text-gray-300 hover:bg-gray-600"
               }`}
             >
-              <tab.icon className="mr-2 h-5 w-5" />
+              <tab.icon className="hidden sm:inline mr-2 h-5 w-5" />
               {tab.label}
             </button>
           ))}
         </div>
 
-          {activeTab === "create" && <CreateProductForm />}
-          {activeTab === "products" && <ProductList />}
-          {activeTab === "analytics" && <AnalyticsTab/>}
-
+        {activeTab === "create" && <CreateProductForm />}
+        {activeTab === "products" && <ProductList />}
+        {activeTab === "analytics" && <AnalyticsTab />}
       </div>
     </div>
   );
